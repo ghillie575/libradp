22db9607f25c58a9feed695d56f833f54cab7dd1
Mikhail Karakou <mikhail@openspm.org>
2025-02-03 17:17:27 +0100

    basic socket functions

---
+++ include/radp/radp.h
@@ -0,0 +1,53 @@
+#include <functional>
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <thread>
+#include <vector>
+#ifndef RADP_H
+#define RADP_H
+namespace ghillie575 {
+    void logClient(int id, std::string message) {
+    std::cout << "[CLIENT " << id << "] " << message << std::endl;
+}
+void logInfo(std::string message) {
+    std::cout << "[INFO] " << message << std::endl;
+}
+void logError(std::string message) {
+    std::cerr << "[ERROR] " << message << std::endl;
+}
+    class RADPServerClient {
+        private:
+            int m_port;
+            
+            int buffer_size = 1024;
+        public:
+            int id;
+            RADPServerClient(int buffer_size);
+            ~RADPServerClient();
+            void handle(int socket);
+
+    };
+    class RADPServer {
+        private:
+            int m_port;
+            int buffer_size = 1024;
+            int server_socket;
+            std::function<void()> onDownload;
+            std::function<int()> onFileRequested;
+            std::vector <RADPServerClient*> clients;
+            struct sockaddr_in serverAddr, clientAddr;
+            socklen_t addrLen = sizeof(clientAddr);
+        public:
+            RADPServer(int port);
+            ~RADPServer();
+            void start();
+            void OnDownloadCallback(std::function<void()> callback);
+            void FileRequestedCallback(std::function<int()> callback);
+
+    };
+    
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+#include <radp/radp.h>
 int main(int, char**){
     std::cout << "Hello, from libradp!\n";
+    ghillie575::RADPServer server(5171);
+    server.start();
 }
